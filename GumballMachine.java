ba422e9e32841f7edc29a9a433a6feda23007cc3
Dream Query <duchnguyen.us@gmail.com>
2018-04-15 12:27:54 -0700

    Java Modified
    by Duc Nguyen

--- GumballMachine.java
+++ GumballMachine.java
@@ -1,25 +1,50 @@
-
-public class GumballMachine
-{
+public class GumballMachine {
 
     private int num_gumballs;
     private boolean has_quarter;
 
-    public GumballMachine( int size )
-    {
+    public GumballMachine(int size) {
         // initialise instance variables
         this.num_gumballs = size;
         this.has_quarter = false;
     }
 
-    public void insertQuarter(int coin)
-    {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+    public void insertQuarter(int coin) {
+        if (coin == 25)
+            this.has_quarter = true;
+        System.out.pringln("you have 25 cents avaiable.");
+        else if (coin == 50)
+            this.has_quarter = true;
+        System.out.println(" you have 50 cents avaiable.");
+        else
+        this.has_quarter = false;
+        System.out.println("Please install a quarter to continue!");
     }
-    
+
+    public void insertOther()
+    {
+        int currentValue = 0;
+        int curretChange = 0;
+
+        if (currentValue = 50) {
+            System.out.println("Thank you for playing")
+            this.has_quarter = true
+        }
+        else if (currentValue < 50) {
+            System.out.println("Please insert more coins until you reach 50 cents")
+            this.has_quarter = true
+        }
+        else if (currentValue > 50) {
+            currentChange = currentValue - 50;
+            System.out.println("Here is your change" + currentChange + "cent(s)");
+            this.has_quarter = true
+        }
+
+        else {
+            this.has_quarter = false;
+        }
+    }
+
     public void turnCrank()
     {
     	if ( this.has_quarter )
@@ -27,7 +52,7 @@ public class GumballMachine
     		if ( this.num_gumballs > 0 )
     		{
     			this.num_gumballs-- ;
-    			this.has_quarter = false ;
+    			this.has_quarter =  false;
     			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
     		}
     		else
@@ -35,9 +60,9 @@ public class GumballMachine
     			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
     		}
     	}
-    	else 
+    	else
     	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+    		System.out.println( "Please insert a quarter." ) ;
+    	}
     }
 }
